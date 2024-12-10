9d154a84519815dfbbd6b24e3636259cbc157b10
dwLG00 <wallace.dylan00@gmail.com>
2024-12-09 23:47:01 -0600

    Added match, match_any, cut, split

--- src/regex_parser.pl
+++ src/regex_parser.pl
@@ -1,17 +1,37 @@
 :- module(regex_parser, [
-    compile/2,
+    re_match/2, re_match_any/2, re_cut/4, re_split/3, re_compile/2,
     parse_and/3, parse_or/3, parse_star/2, parse_maybe/2, parse_char/2, parse_end/3, parse_nothing/3,
     parse_and_aux/5, parse_or_aux/5, parse_star_aux/4, parse_maybe_aux/4, parse_char_aux/4
 ]).
 :- use_module(library(clpfd)).
 :- use_module(library(list_util)).
 
+% matching regex
+re_match(Regex, Xs) :- call(Regex, Xs, _, []).
+
+re_match_any(Regex, Xs) :- % matches any portion of the text
+    append(_, Rest, Xs),
+    call(Regex, Rest, _, _).
+
+re_cut(Regex, Xs, Before, After) :-
+    append(Before, Rest, Xs),
+    call(Regex, Rest, _, After).
+
+re_split(Regex, Xs, SplitValues) :-
+    re_cut(Regex, Xs, Before, After) -> (
+        re_split(Regex, After, SplitValues2),
+        SplitValues = [Before|SplitValues2]
+    ); (
+        SplitValues = [Xs]
+    ).
+    
+
 % compile regex
 
-compile([], parse_nothing).
-compile(Xs, Parse) :-
+re_compile([], parse_nothing).
+re_compile(Xs, Regex) :-
     compile_aux(Xs, RevStack, [], []),
-    combine_terms(RevStack, Parse).
+    combine_terms(RevStack, Regex).
 
 compile_aux([], [], _, []).
 compile_aux([')'|Rest], [], _, Rest).
